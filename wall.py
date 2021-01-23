d5f84f069b0d027ee5ef45e2a03d8a00bff9761a
Tusharagarwal8955 <52047488+Tusharagarwal8955@users.noreply.github.com>
2021-01-23 21:47:10 +0530

    Update wall.py

--- wall.py
+++ wall.py
@@ -7,30 +7,31 @@ import ctypes
 SPI_SETDESKWALLPAPER = 20
 
 
-def get_wallpaper():
-	access_key = 'K7r6MdenUXTqJzFwiogTUWQqEi9Y_4brWKg41yYIr64' # add your unspash api key here
-	url = 'https://api.unsplash.com/photos/random?client_id=' + access_key
-	params = {
-		'query': 'HD wallpapers',
-		'orientation': 'landscape'
-	}
+def get_wallpaper(query):
+    access_key = 'K7r6MdenUXTqJzFwiogTUWQqEi9Y_4brWKg41yYIr64' # add your unspash api key here
+    url = 'https://api.unsplash.com/photos/random?client_id=' + access_key
+    params = {
+        'query': query,
+        'orientation': 'landscape'
+    }
+    response = requests.get(url, params=params).json()
+    image_source = response['urls']['full']
 
-	response = requests.get(url, params=params).json()
-	image_source = response['urls']['full']
+    image = wget.download(image_source, 'F:/Wallpapers/wallpaper.jpg') # add the path here
+    return image
 
-	image = wget.download(image_source, 'F:/Wallpapers/wallpaper.jpg') # add the path here
-	return image
-
-def change_wallpaper():
-	wallpaper = get_wallpaper()
-	ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, wallpaper , 0) # add the path  here as well
+def change_wallpaper(query):
+    print('This App is Made by Tushar Agrawal\n')
+    wallpaper = get_wallpaper(query)
+    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, wallpaper , 0) # add the path  here as well
 
 def main():
-	print('Your wallpaper will be downloaded and applied ')
-	try:
-		change_wallpaper()
-		print('Made by Tushar Agrawal')
-	except e:
-		print(e)
+    print('Please wait your wallpaper will be downloaded and applied \n')
+    try:
+        query = input('what kind of wallpaper do you want ?\n')
+        change_wallpaper(query)
+    except e:
+        print(e)
 if __name__ == "__main__":
-	main()
+    main()
+    input("Press enter to exit ;)")
